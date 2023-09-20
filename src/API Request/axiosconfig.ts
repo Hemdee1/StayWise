d9f670e1b962e1943dc75d6ecdf9442c008d4a36
hemdee <sanusi5110@gmail.com>
2023-09-20 06:17:16 +0100

    update config

--- src/API Request/axiosconfig.ts
+++ src/API Request/axiosconfig.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const url =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000"
+    : "https://staywise.onrender.com";
+
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: url,
 });
 
 export default axiosInstance;
